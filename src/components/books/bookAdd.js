c9fa57122127fa9fbe1624da45dfd9c830aff5bb
Ermi <manermidem@gmail.com>
2023-03-01 09:24:16 +0300

    Add functionality to add books into the app

--- src/components/books/bookAdd.js
+++ src/components/books/bookAdd.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/booksSlice';
 import './books.css';
 
 function BookAdd() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const book = {
+    item_id: '',
+    title: '',
+    author: '',
+  };
+
+  const resetInput = () => {
+    setTitle('');
+    setAuthor('');
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      dispatch(
+        addBook({
+          ...book,
+          item_id: uuidv4(),
+          title,
+          author,
+        }),
+      );
+      resetInput();
+    }
+  };
+
   return (
     <div>
       <h2 className="formheading">ADD NEW BOOK</h2>
-      <form className="form">
+      <form onSubmit={submitHandler} className="form">
         <input
           className="inpField"
           type="text"
           placeholder="Book Title"
           name="Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
         />
         <input
           className="inpField"
           type="text"
           placeholder="Book Author"
           name="Author"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+          required
         />
         <button className="btn" type="submit">
           ADD BOOK
