b270ecfdfee6015edc99023d398ae5094ab37837
Ermi <manermidem@gmail.com>
2023-03-01 09:27:32 +0300

    Update category by adding some functionality

--- src/components/books/categories.js
+++ src/components/books/categories.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function Category() {
+  const { category } = useSelector((state) => state.category);
+  const [searchField, setSearchField] = useState('');
+  const [filterResult, setFilterResult] = useState(category);
+
+  const filterHandler = () => {
+    setFilterResult(
+      filterResult.map((item) => item.country.includes(searchField)),
+    );
+  };
+
   return (
-    <button className="cat" type="button">
-      Check status
-    </button>
+    <div className="categories">
+      <input
+        type="text"
+        value={searchField}
+        onChange={(e) => setSearchField(e.target.value)}
+      />
+      <button className="cat" type="button" onClick={filterHandler}>
+        Filter
+      </button>
+    </div>
   );
 }
 
