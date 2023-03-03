3780c79a1115ccf13928ff873eb04f4812eaa700
Ermi <manermidem@gmail.com>
2023-03-03 14:26:16 +0300

    Add book lists to the UI

--- src/components/books/homePage.js
+++ src/components/books/homePage.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookAdd from './bookAdd';
 import BookList from './bookList';
+import { getBookData } from '../../redux/books/booksSlice';
 
 function Home() {
-  const { listBooks } = useSelector((state) => state.book);
+  const dispatch = useDispatch();
+  const { listBooks, isLoading } = useSelector((state) => state.book);
+
+  useEffect(() => {
+    dispatch(getBookData());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (listBooks.length === 0) {
+    return (
+      <>
+        <h1 className="no-book">No Books to Display</h1>
+        <hr className="hr" />
+        <BookAdd />
+      </>
+    );
+  }
+  // const keys = Object.keys(listBooks);
   return (
     <div className="parent">
       {listBooks.map((book) => (
-        <BookList key={book.item_id} List={book} />
+        <BookList key={book.item_id} List={book} id={book.item_id} />
       ))}
       <hr className="hr" />
       <BookAdd />
